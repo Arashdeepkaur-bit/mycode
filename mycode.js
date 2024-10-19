52710a67bc2ebad94e8b18ed18098f5bef3252ea
Arash <sainiarash555@gmail.com>
2024-10-19 14:13:16 +0530

    mycode

---
+++ mycode.js
@@ -0,0 +1,10 @@
+const PI= 3.14;
+let radius;
+
+let circumference;
+document.getElementById("button").onclick=function(){
+    radius=document.getElementById("input").value;
+    radius=Number(radius);
+    circumference=2 * PI * radius;
+    document.getElementById("div").textContent=circumference;
+}
