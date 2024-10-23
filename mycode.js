c7e9eaf7d5e002271a78fc2c8cb5af4045bf31ed
Arash <sainiarash555@gmail.com>
2024-10-23 14:13:57 +0530

    mycode

--- mycode.js
+++ mycode.js
@@ -1,9 +1,9 @@
 const PI= 3.14;
-let radius;
+// let radius;
 
 let circumference;
 document.getElementById("button").onclick=function(){
-    radius=document.getElementById("input").value;
+   let radius=document.getElementById("input").value;
     radius=Number(radius);
     circumference=2 * PI * radius;
     document.getElementById("div").textContent=circumference;
---
+++ mycode2.js
@@ -0,0 +1,51 @@
+//arithmetic operation
+// let student=27;
+// student=student+1;
+// student=student-1;
+// student=student*2;
+// student=student/3;
+// student=student%2;
+
+// student+=2;
+// student-=2;
+// student*=3;
+// student/=9;
+// student%=9;
+// student++;
+// student**=2;
+// console.log(student)
+ // operators precedence
+//  1.parameters
+//  2 exponents
+//  3 multi divide Modulo
+//  4 add sub   
+// let a=1+2*4/6*(4-2)
+// console.log(a)
+// there is 2 way to print user Input
+// 1 window prompt
+// 2 html textcontent
+// let first=window.prompt("enter name")
+// console.log(first)
+//  let a;
+// document.getElementById("button").onclick=function(){
+// a = document.getElementById("input").value;
+//  document.getElementById("h1").textContent=`hello ${a}`;
+
+// }
+
+// let inc=document.getElementById("inc");
+// let reset=document.getElementById("reset");
+// let dec=document.getElementById("dec");
+// let count=0;
+// inc.onclick=function(){
+//     count++;
+//     document.getElementById("h1").innerHTML=count;
+// }
+// dec.onclick=function(){
+//     count--;
+//     document.getElementById("h1").innerHTML=count;
+// }
+// reset.onclick=function(){
+//     count=0;
+//     document.getElementById("h1").innerHTML=count;
+// }
